c87159a7a67296da2bf00b9b1769cc66bfb61dbd
Anfoshi <ahmeddawood666680@gmail.com>
2021-07-27 18:56:36 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+'''
+Name: Ahmed Elshahat Saad Abdelazeez Dawood
+Task: Enozom Internship Technical Task
+'''
+
+import hashlib
+
+# encoding GeeksforGeeks using encode()
+# then sending to md5()
+def MD5(str2hash):
+    result = hashlib.md5(str2hash.encode())
+
+    return f"The hexadecimal equivalent of hash is : {result.hexdigest().upper()}"
+
+f=open('annual-enterprise-survey-2020-financial-year-provisional-csv.csv','r')
+cnt=0
+concatenated_string=''
+
+for i in f.readlines()[1:]:                          # skipping the first row of column names
+    cnt += 1
+    if cnt%2==1:                                     # choosing the odd numbered rows
+        concatenated_string+=i.split(',')[2]         # choosing the 3rd column of each row
+
+f.close()
+
+print(concatenated_string)                           # printing the string
+print(MD5(concatenated_string))                      # calling the MD5 function
